c359db4158bfe736bc8f534ea26b59813ad94770
maiconrcf <maiconrcf@gmail.com>
2014-03-10 14:33:13 -0300

    #refact inherit from elements

--- elemento.js
+++ elemento.js
@@ -1,43 +1,23 @@
 function Elemento ()
 {
-	this.x = 0;
-	this.y = 0;
+  this.x = 0;
+  this.y = 0;
 
-	this.width = 0;
-	this.height = 0;
-	
-	this.face = 0;
-}
+  this.width = 0;
+  this.height = 0;
 
-Elemento.prototype.setWidth = function(width)
-{
-	this.width = width;
-}
-
-Elemento.prototype.setHeight = function(height)
-{
-	this.height = height;
-}
-
-Elemento.prototype.setPosX = function(x)
-{
-	this.x = x;
-}
-
-Elemento.prototype.setPosY = function(y)
-{
-	this.y = y;
+  this.face = 0;
 }
 
 //desenha na tela
 Elemento.prototype.drawElement = function (context)
 {
-	context.beginPath();
-	context.rect(this.x, this.y, this.width, this.height);
-	context.lineWidth = 1;
-    context.strokeStyle = 'pink';
-    context.stroke();
-	context.closePath();
-	
-	context.drawImage(this.image, this.face, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+  context.beginPath();
+  context.rect(this.x, this.y, this.width, this.height);
+  context.lineWidth = 1;
+  context.strokeStyle = 'pink';
+  context.stroke();
+  context.closePath();
+
+  context.drawImage(this.image, this.face, 0, this.width, this.height, this.x, this.y, this.width, this.height);
 }
--- stick.js
+++ stick.js
@@ -1,8 +1,13 @@
 //stick
 function Stick ()
 {
-	this.image = new Image();
-	this.image.src = "images/stick1.png";
+  this.image = new Image();
+  this.image.src = "images/stick1.png";
+
+  this.width = 32;
+  this.height = 32;
+  this.x = 0;
+  this.y = 0;
 }
 
 Stick.prototype = new Elemento();
@@ -10,26 +15,26 @@ Stick.prototype.constructor = Stick;
 
 Stick.prototype.setImg = function(img)
 {
-	this.image.src = img;
+  this.image.src = img;
 }
 
 Stick.prototype.randomPos = function ()
 {
-	var y = Math.floor((Math.random()* (documentHeight - this.height) )+1);
-	var x = Math.floor((Math.random()* (documentWidth - this.width) )+1);
-	
-	this.setPosX(x);
-	this.setPosY(y);
+  var y = Math.floor((Math.random()* (documentHeight - this.height) )+1);
+  var x = Math.floor((Math.random()* (documentWidth - this.width) )+1);
+
+  this.x = x;
+  this.y = y;
 }
 
 Stick.prototype.intervalChangePosition = function(obj, interval)
 {
-	setInterval
-	(
-		function()
-		{
-			obj.randomPos();
-		},
-		interval
-	);
+  setInterval
+  (
+    function()
+    {
+      obj.randomPos();
+    },
+    interval
+  );
 }
